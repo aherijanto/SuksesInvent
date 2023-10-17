e9696da4f9a6bc1640bd603d6097b71b3d2d219b
bistique <aherijanto@gmail.com>
2023-10-17 11:35:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/suksesinvent/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.suksesinvent.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Sukses Plastik");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
